033c04e210f16efdefa1ebcd597163d52276bf66
Sergey <metrolog_omsk@mail.ru>
2022-06-18 19:53:01 +0500

    Решил задачу № 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -2,3 +2,33 @@
 // 14212 -> нет
 // 23432 -> да
 // 12821 -> да
+
+Console.Clear();
+Console.WriteLine("*******************************************");
+Console.Write("Даны пятизначные числа: ");
+
+int N1 = 14212;
+int N2 = 23432;
+int N3 = 12821;
+
+Console.WriteLine($"{N1} {N2} {N3}");
+
+void checkPol (int number)
+{
+   
+    if ((number / 10000 == number % 10) && ((number / 1000) % 10 == (number % 100) / 10))
+    {
+        Console.WriteLine($"Число {number} является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является полиндромом");
+    }
+
+}
+
+checkPol(N1);
+checkPol(N2);
+checkPol(N3);
+
+Console.WriteLine("*******************************************");
