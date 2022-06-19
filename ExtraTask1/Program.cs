ade662cb2f00685fb0ff4965183412146f183a92
Sergey <metrolog_omsk@mail.ru>
2022-06-19 11:58:09 +0500

    Решил доп. задачу № 1

--- ExtraTask1/Program.cs
+++ ExtraTask1/Program.cs
@@ -1,3 +1,23 @@
 ﻿// Задача 1. Рассчитать значение y при заданном x по формуле
 // y = sin(2 степени)x, при x > 0
 // y = 1 - 2sinx(2 степени) в противном случае
+
+Random rand = new Random();
+int x = rand.Next(1, 10);
+Console.Clear();
+Console.WriteLine("*******************************************");
+double y;
+
+Console.WriteLine($"Дано значение x равное: {x}");
+
+if (x > 0)
+{
+y = Math.Round(Math.Pow(Math.Sin(x), 2), 2);
+}
+else
+{
+y = Math.Round(1 - 2 * Math.Sin(Math.Pow(x, 2)), 2);
+}
+
+Console.WriteLine(y);
+Console.WriteLine("*******************************************");
--- ExtraTask2/Program.cs
+++ ExtraTask2/Program.cs
@@ -1,2 +1,20 @@
 ﻿// Задача 2. Дано трёх значное число N. 
 // Определить кратна ли трём сумма всех его цифр.
+
+Random rand = new Random();
+int N = rand.Next(1, 10);
+Console.WriteLine($"Дано число N равное: {N}");
+
+int sum;
+
+int[] MakeArray(int number)
+{
+    int[] arrayValue = new int[];
+    int del = 100;
+    for (int i = 0; i < Convert.ToString.number.Length; i++)
+    {
+        arrayValue[i] = number / del;
+        del = del / 10;
+    }
+}
+
