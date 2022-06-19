28e039b9fdd176bbee0b7bc4cd9ec3908039a227
Sergey <metrolog_omsk@mail.ru>
2022-06-19 22:33:21 +0500

    Решил доп. задачу № 2(проверить правильность)

--- ExtraTask2/Program.cs
+++ ExtraTask2/Program.cs
@@ -1,20 +1,34 @@
 ﻿// Задача 2. Дано трёх значное число N. 
 // Определить кратна ли трём сумма всех его цифр.
 
+Console.Clear();
 Random rand = new Random();
-int N = rand.Next(1, 10);
+int N = rand.Next(100, 1000);
+Console.WriteLine("*******************************************");
 Console.WriteLine($"Дано число N равное: {N}");
 
-int sum;
+int[] array = new int[3];
 
-int[] MakeArray(int number)
+MakeArray(array, N);
+int sum = array[0] + array[1] + array[2];
+
+Console.WriteLine($"Сумма всех чисел числа равна: {sum}");
+if (sum % 3 == 0)
 {
-    int[] arrayValue = new int[];
-    int del = 100;
-    for (int i = 0; i < Convert.ToString.number.Length; i++)
+    Console.WriteLine($"Сумма {sum} кратна числу 3");
+}
+else
+{
+    Console.WriteLine($"Сумма {sum} не кратна числу 3");
+}
+
+void MakeArray(int[] currentArray, int value)
+{
+    for (int i = array.Length - 1; i >= 0; i--)
     {
-        arrayValue[i] = number / del;
-        del = del / 10;
+        currentArray[i] = value % 10;
+        value = value / 10;
     }
 }
+Console.WriteLine("*******************************************");
 
