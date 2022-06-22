de35d95723ec0edb4f409b7fdff2f676b0e79153
Sergey <metrolog_omsk@mail.ru>
2022-06-22 20:53:07 +0500

    Решил доп. задачу № 4

--- ExtraTask4/Program.cs
+++ ExtraTask4/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача 4. Дан массив длиной 10 элементов. 
 // Заполнить его числами от 1 до 10.
+
+Random rand = new Random();
+int[] array = new int[10];
+
+Console.Clear();
+Console.WriteLine("Заполним массив числами от 1 до 10");
+Console.WriteLine("**************************************************************************");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(1, 11);
+    Console.Write($"{array[i]} \t");
+}
+Console.WriteLine();
+Console.WriteLine("**************************************************************************");
