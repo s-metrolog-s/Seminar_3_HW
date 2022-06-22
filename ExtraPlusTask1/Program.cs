324f24d2215ef60e3b9e7ad1db5701bc7a712924
Sergey <metrolog_omsk@mail.ru>
2022-06-22 22:15:01 +0500

    Начал решать доп. сложн. задачу № 1

---
+++ ExtraPlusTask1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 1. На ввод подаётся номер четверти. 
+// Создаются 3 случайные точки в этой четверти. 
+// Определите самый оптимальный маршрут для 
+// торгового менеджера, который выезжает из центра координат.
+
+
+Random rand = new Random();
+Console.WriteLine("Определение оптимального маршрута менеджера");
+Console.WriteLine("*******************************************");
+int quarter = rand.Next(1, 5);
+Console.WriteLine($"Выбрана {quarter} четверть");
+
+int[] arrayA  = new int[2];
+int[] arrayB  = new int[2];
+int[] arrayC = new int[2];
+
+int maxX = 0;
+int maxY = 0;
+
+int maxArea = 100; //rand.Next(10, 1000);
+
+if (quarter == 1) maxX = maxArea; maxY = maxArea;
+if (quarter == 2) maxX = -maxArea; maxY = maxArea;
+if (quarter == 3) maxX = -maxArea; maxY = -maxArea;
+if (quarter == 4) maxX = maxArea; maxY = -maxArea;
+
+PutPoint(arrayA, maxX, maxY);
+PutPoint(arrayB, maxX, maxY);
+PutPoint(arrayB, maxX, maxY);
+
+void PutPoint(int[] currentArray, int X, int Y)
+{
+    currentArray[0] = rand.Next(0, X); 
+    currentArray[1] = rand.Next(0, Y); 
+}
+
+Console.WriteLine($"{arrayA[0]};{arrayA[1]}");
+Console.WriteLine($"{arrayB[0]};{arrayB[1]}");
+Console.WriteLine($"{arrayC[0]};{arrayC[1]}");
