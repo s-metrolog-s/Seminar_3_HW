d377b9a5c44ea2a8eb09a20c31175f163faefdbf
Sergey <metrolog_omsk@mail.ru>
2022-06-22 20:41:15 +0500

    Решил доп. задачу № 3

--- ExtraTask3/Program.cs
+++ ExtraTask3/Program.cs
@@ -16,16 +16,21 @@ void FindValue(int[] currentArray, int value1, int value2)
 {
     bool flag1 = false;
     bool flag2 = false;
+    string name = $"{currentArray[0]}{currentArray[1]}{currentArray[2]}";
     for (int i = 0; i < currentArray.Length; i++)
     {
         if (currentArray[i] == value1) flag1 = true;
         if (currentArray[i] == value2) flag2 = true;
     }
     
-    if (flag1 == true && flag2 == true) Console.WriteLine($"Число содержит {value1} и {value2}");
-    else if (flag1) Console.WriteLine($"Число содержит {value1}");
-    else if (flag2) Console.WriteLine($"Число содержит {value2}");
-    else Console.WriteLine($"Число не содержит чисел {value1} и {value2}");
+    if (flag1 == true && flag2 == true) Console.WriteLine($"Число {name} содержит значение {value1} и {value2}");
+    else if (flag1) Console.WriteLine($"Число {name} содержит значение {value1}");
+    else if (flag2) Console.WriteLine($"Число {name} содержит значение {value2}");
+    else
+    {
+    Console.WriteLine($"Число {name} не содержит значений {value1} и {value2}");
+    }
+    Console.WriteLine("************************************************");
 }
 
 int[] array = new int[3];
@@ -34,8 +39,10 @@ int N2 = 547;
 int N3 = 127;
 int N4 = 345;
 
-Console.WriteLine($"Даны трехзначные числа: {N1} {N2} {N3}");
-Console.WriteLine("Начинаем поиск чисел 4 и 7...");
+Console.Clear();
+Console.WriteLine($"Даны трехзначные числа: {N1} - {N2} - {N3} - {N4}");
+Console.WriteLine("Начинаем поиск чисел 4 и 7 в указанных числах...");
+Console.WriteLine("************************************************");
 
 MakeArray(array, N1);
 FindValue(array, 4, 7);
