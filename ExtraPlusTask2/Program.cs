c6dfe7e55c5278d6420a7d6fdb5a3c76a2c8f4b1
Sergey <metrolog_omsk@mail.ru>
2022-06-28 21:39:10 +0500

    Решил доп. повыш. задачу № 2

--- ExtraPlusTask2/Program.cs
+++ ExtraPlusTask2/Program.cs
@@ -18,13 +18,33 @@ PutPoint(arrayB);
 PutPoint(arrayC);
 PutPoint(arrayD);
 
-Console.WriteLine($"Координаты точки A: [{arrayA[0]} ; {arrayA[1]}]");
-Console.WriteLine($"Координаты точки B: [{arrayB[0]} ; {arrayB[1]}]");
-Console.WriteLine($"Координаты точки C: [{arrayC[0]} ; {arrayC[1]}]");
-Console.WriteLine($"Координаты точки D: [{arrayD[0]} ; {arrayD[1]}]");
+Console.WriteLine($"Координаты точки A: [{arrayA[0]} ; {arrayA[1]}]"); // x1    y1
+Console.WriteLine($"Координаты точки B: [{arrayB[0]} ; {arrayB[1]}]"); // x2    y2
+Console.WriteLine($"Координаты точки C: [{arrayC[0]} ; {arrayC[1]}]"); // x3    y3
+Console.WriteLine($"Координаты точки D: [{arrayD[0]} ; {arrayD[1]}]"); // x4    y4
+Console.WriteLine("*******************************************");
+
+Console.WriteLine($"Вектора AB и CD {CrossingVectors(arrayA, arrayB, arrayC, arrayD)}");
+Console.WriteLine("*******************************************");
 
 void PutPoint(int[] currentArray)
 {
-    currentArray[0] = rand.Next(-100, 100); 
-    currentArray[1] = rand.Next(-100, 100);
+    currentArray[0] = rand.Next(-10, 10); 
+    currentArray[1] = rand.Next(-10, 10);
+}
+
+int vectorMult(int ax,int ay,int bx,int by) //векторное произведение
+{
+    return ax * by - bx * ay;
+}                     
+
+string CrossingVectors (int[] array1, int[] array2, int[] array3, int[] array4) //проверка пересечения векторов
+{                                                       
+    int v1 = vectorMult(arrayD[0] - arrayC[0], arrayD[1] - arrayC[1], arrayA[0] - arrayC[0], arrayA[1] - arrayC[1]);
+    int v2 = vectorMult(arrayD[0] - arrayC[0], arrayD[1] - arrayC[1], arrayB[0] - arrayC[0], arrayB[1] - arrayC[1]);
+    int v3 = vectorMult(arrayB[0] - arrayA[0], arrayB[1] - arrayA[1], arrayC[0] - arrayA[0], arrayC[1] - arrayA[1]);
+    int v4 = vectorMult(arrayB[0] - arrayA[0], arrayB[1] - arrayA[1], arrayD[0] - arrayA[0], arrayD[1] - arrayA[1]);
+    if ((v1 * v2) < 0 && (v3 * v4) < 0)
+    return "пересекаются";
+    else return "не пересекаются";
 }
