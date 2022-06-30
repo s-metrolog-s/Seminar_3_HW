082fc575249c1ef3810536f5394c5cf1d9e4a103
Sergey <metrolog_omsk@mail.ru>
2022-06-30 23:08:39 +0500

    Начал решать дополнительную повышен. задачу № 7

--- ExtraPlusTask7/Program.cs
+++ ExtraPlusTask7/Program.cs
@@ -1,3 +1,26 @@
 ﻿// Задача 7. Массив из ста элементов заполняется случайными числами от 1 до 100. 
-//Удалить из массива все элементы, содержащие цифру 3. 
+// Удалить из массива все элементы, содержащие цифру 3. 
 // Вывести в консоль новый массив и количество удалённых элементов.
+
+Random rand = new Random();
+int[] array = new int[100];
+
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        currentArray[i] = rand.Next(1, 100);
+    }
+}
+
+void PrintArray(int[] currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if ((i + 1) % 20 == 1) Console.WriteLine();
+        Console.Write($"{currentArray[i]}\t");
+    }
+}
