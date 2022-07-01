c20ad4b80b5a6e59ab10ee39e54c1975aed8ca51
Sergey <metrolog_omsk@mail.ru>
2022-07-01 07:17:25 +0500

    Решил  дополнительную повышен. задачу № 7

--- ExtraPlusTask7/Program.cs
+++ ExtraPlusTask7/Program.cs
@@ -3,10 +3,24 @@
 // Вывести в консоль новый массив и количество удалённых элементов.
 
 Random rand = new Random();
-int[] array = new int[100];
+int[] arrayBefore = new int[100];
 
-FillArray(array);
-PrintArray(array);
+Console.Clear();
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Заполним массив случайными числами:");
+FillArray(arrayBefore);
+PrintArray(arrayBefore);
+
+Console.WriteLine("***************************************************************************");
+ChangeNumbers(arrayBefore); // Заменим все элементы с 3 на "-1"
+
+int countDel = CountNumbers(arrayBefore); // Подсчет количества элементов с 3
+int[] arrayAfter = new int[arrayBefore.Length - countDel];
+FillArrayWithoutSymbol(arrayBefore, arrayAfter);
+Console.WriteLine($"Удаляем {countDel} элемент (-а, -ов), которые содержать цифру 3");
+
+PrintArray(arrayAfter);
+Console.WriteLine("***************************************************************************");
 
 void FillArray(int[] currentArray)
 {
@@ -20,7 +34,42 @@ void PrintArray(int[] currentArray)
 {
     for (int i = 0; i < currentArray.Length; i++)
     {
-        if ((i + 1) % 20 == 1) Console.WriteLine();
+        if ((i + 1) % 10 == 1) Console.WriteLine();
         Console.Write($"{currentArray[i]}\t");
+    }
+    Console.WriteLine();
+}
+
+void ChangeNumbers(int []currentArray)
+{
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] / 10 == 3 || currentArray[i] % 10 == 3)
+        {
+            currentArray[i] = - 1;
+        }
+    }
+}
+
+int CountNumbers(int []currentArray)
+{
+    int count = 0;
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] == - 1) count++;
+    }
+    return count;
+}
+
+void FillArrayWithoutSymbol(int[] currentArray1, int[] currentArray2)
+{
+    int j = 0;
+    for (int i = 0; i < currentArray1.Length; i++)
+    {
+        if (currentArray1[i] != - 1)
+        {
+        currentArray2[j] = currentArray1[i];
+        j = j + 1;
+        }
     }
 }
